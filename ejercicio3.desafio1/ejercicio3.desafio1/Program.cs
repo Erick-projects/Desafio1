b9ddfaac0ab86bb122acddce709f430bc257ceb5
Erick-projects <125150013+Erick-projects@users.noreply.github.com>
2023-02-13 10:14:32 -0600

    Update Program.cs

--- ejercicio3.desafio1/ejercicio3.desafio1/Program.cs
+++ ejercicio3.desafio1/ejercicio3.desafio1/Program.cs
@@ -33,45 +33,58 @@ namespace ejercicio3.desafio1
 
             if (entrada1nand == "F" && entrada2nand == "V" || entrada1nand == "f" && entrada2nand == "v" || entrada1nand == "F" && entrada2nand == "v" || entrada1nand == "f" && entrada2nand == "V")
             {
-                Console.WriteLine("El resultado de la compuerta logica es: V");
+                Console.WriteLine("El resultado de la compuerta logica NAND es: V");
             }
 
             if (entrada1nand == "V" && entrada2nand == "F" || entrada1nand == "v" && entrada2nand == "f" || entrada1nand == "V" && entrada2nand == "f" || entrada1nand == "v" && entrada2nand == "F")
             {
-                Console.WriteLine("El resultado de la compuerta logica es: V");
+                Console.WriteLine("El resultado de la compuerta logica NAND es: V");
             }
 
             if (entrada1nand == "V" && entrada2nand == "V" || entrada1nand == "v" && entrada2nand == "v" || entrada1nand == "V" && entrada2nand == "v" || entrada1nand == "v" && entrada2nand == "V")
             {
-                Console.WriteLine("El resultado de la compuerta logica es: F");
+                Console.WriteLine("El resultado de la compuerta logica NAND es: F");
             }
 
             if (entrada1nand == "F" && entrada2nand == "F" || entrada1nand == "f" && entrada2nand == "f" || entrada1nand == "F" && entrada2nand == "f" || entrada1nand == "f" && entrada2nand == "F")
             {
-                Console.WriteLine("El resultado de la compuerta logica es: V");
+                Console.WriteLine("El resultado de la compuerta logica NAND es: V");
             }
 
             if (entrada1xor == "F" && entrada2xor == "F" || entrada1xor == "f" && entrada2xor == "f" || entrada1xor == "F" && entrada2xor == "f" || entrada1xor == "f" && entrada2xor == "F")
             {
-                Console.WriteLine("El resultado de la compuerta logica es: F");
+                Console.WriteLine("El resultado de la compuerta logica XOR es: F");
             }
 
             if (entrada1xor == "F" && entrada2xor == "V" || entrada1xor == "f" && entrada2xor == "v" || entrada1xor == "F" && entrada2xor == "v" || entrada1xor == "f" && entrada2xor == "V")
             {
-                Console.WriteLine("El resultado de la compuerta logica es: V");
+                Console.WriteLine("El resultado de la compuerta logica XOR es: V");
             }
 
             if (entrada1xor == "V" && entrada2xor == "F" || entrada1xor == "v" && entrada2xor == "f" || entrada1xor == "V" && entrada2xor == "f" || entrada1xor == "v" && entrada2xor == "F")
             {
-                Console.WriteLine("El resultado de la compuerta logica es: V");
+                Console.WriteLine("El resultado de la compuerta logica XOR es: V");
             }
 
             if (entrada1xor == "V" && entrada2xor == "V" || entrada1xor == "v" && entrada2xor == "v" || entrada1xor == "V" && entrada2xor == "v" || entrada1xor == "v" && entrada2xor == "V")
             {
-                Console.WriteLine("El resultado de la compuerta logica es: F");
+                Console.WriteLine("El resultado de la compuerta logica XOR es: F");
             }
 
 
+            Console.WriteLine("\n");
+            Console.Write("\t");
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("\t*                                         *");
+            Console.WriteLine("\t*   Este programa fue diseñado por:       *");
+            Console.WriteLine("\t*                                         *");
+            Console.WriteLine("\t*          Erick Magaña                   *");
+            Console.WriteLine("\t*                                         *");
+            Console.Write("\t");
+            Console.WriteLine("*********************************************");
+            Console.Write("\t");
+            Console.WriteLine("--> Fin del Programa");
+            Console.Write("\t");
             Console.ReadKey();
         }
     }
